fe77a4c005686dc385ade4558db625115723b7f4
Antonina Grebeniuk <rebel.t2808@gmail.com>
2024-07-06 21:39:31 +0400

    add a solution

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,24 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    let contacts = JSON.parse(data);
+
+    if (contacts.length > 0) {
+      contacts.pop();
+      await fs.writeFile(
+        PATH_DB,
+        JSON.stringify(contacts, undefined, 2),
+        'utf-8',
+      );
+    } else {
+      console.log('No contacts to remove.');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeLastContact();
